7b0b03d320e72cdf54a7f8e99a1fc71bab358242
oldcapone <ivanov.planeta@gmail.com>
2022-12-12 16:52:59 +0300

    ADD CBV

--- specsmart/blogapp/urls.py
+++ specsmart/blogapp/urls.py
@@ -6,11 +6,17 @@ from django.conf.urls.static import static
 app_name = 'blogapp'
 
 urlpatterns = [
-    path('', views.main_view, name='index'),
-    path('contacts/', views.contact_form, name='contacts'),
-    path('about/', views.about, name='about'),
-    path('elements/', views.elements, name='elements'),
-    path('blogs/', views.blog, name='blogs'),
-    path('blog/', views.elements, name='blog'),
-    path('post/<int:id>/', views.post, name='post'),
+    # path('', views.main_view, name='index'),
+    path('', views.MainView.as_view (), name='index'),
+
+    path('contacts/', views.contact_view, name='contacts'),
+    path('about/', views.AboutDetailView.as_view (), name='about'),
+    path('elements/', views.ElementsView.as_view (), name='elements'),
+    path('blogs/', views.BlogsView.as_view (), name='blogs'),
+    path('blog/', views.BlogView.as_view (), name='blog'),
+    path('post/<int:id>/', views.PostView.as_view (), name='post'),
+    # path('post/<int:id>/', views.post, name='post'),
+    path('tag-list', views.TagListView.as_view (), name='tag_list'),
+    path('tag-detail/<int:pk>/', views.TagDetailView.as_view (), name='tag_detail'),
+    path('tag-create/', views.TagCreateView.as_view (), name='tag_create'),
 ]
--- specsmart/blogapp/views.py
+++ specsmart/blogapp/views.py
@@ -1,34 +1,74 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .models import Post, Tag
 from .forms import ContactForm
 from django.core.mail import send_mail
+from django.views.generic import ListView, DetailView, CreateView, UpdateView,TemplateView
+from django.views.generic.base import ContextMixin
 
 
+class NameContextMixin(ContextMixin):
 
-def main_view(request):
-    posts = Post.objects.all()
-    return render(request, 'blogapp/index.html', context={'posts': posts})
+    def get_context_data(self, *args, **kwargs):
+        """
+        Отвечает за передачу параметров в контекст
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        context = super().get_context_data(*args, **kwargs)
+        context['name'] = 'Теги'
+        return context
 
-def about(request):
-    return render(request, 'blogapp/about.html')
+class MainView(ListView, NameContextMixin):
+    model = Post
+    template_name = 'blogapp/index.html'
 
-def elements(request):
-    return render(request, 'blogapp/elements.html')
+    def get_queryset(self):
+        """
+        Получение данных
+        :return:
+        """
+        return Post.objects.all()
 
-def blogs(request):
-    return render(request, 'blogapp/blog.html')
+class AboutDetailView(TemplateView):
+    template_name = 'blogapp/about.html'
 
-def blog(request):
-    return render(request, 'blogapp/single-blog.html')
+class ElementsView(TemplateView):
+    template_name = 'blogapp/elements.html'
 
-def post(request, id):
-    post = get_object_or_404(Post, id=id)
-    return render(request, 'blogapp/post.html', context={'post': post})
 
+class BlogsView(TemplateView):
+    template_name = 'blogapp/blog.html'
+
+class BlogView(TemplateView):
+    template_name = 'blogapp/single-blog.html'
+
+class PostView(DetailView, NameContextMixin):
+    model = Post
+    template_name = 'blogapp/post.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Метод обработки get запроса
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self.post_id = kwargs['id']
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """
+        Получение этого объекта
+        :param queryset:
+        :return:
+        """
+        return get_object_or_404(Post, id=self.post_id)
 
 # noinspection PyUnusedLocal
-def contact_form(request):
+def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -51,3 +91,82 @@ def contact_form(request):
     else:
         form = ContactForm()
         return render(request, 'blogapp/contact.html', context={'form': form})
+
+
+# CRUD CREATE, READ (LIST, DETAIL), UPDATE, DELETE
+# список тегов
+class TagListView(ListView, NameContextMixin):
+    model = Tag
+    template_name = 'blogapp/tag_list.html'
+    context_object_name = 'Теги'
+
+    def get_queryset(self):
+        """
+        Получение данных
+        :return:
+        """
+        return Tag.objects.all()
+
+# детальная информация
+# детальная информация
+class TagDetailView(DetailView, NameContextMixin):
+    model = Tag
+    template_name = 'blogapp/tag_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Метод обработки get запроса
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self.tag_id = kwargs['pk']
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """
+        Получение этого объекта
+        :param queryset:
+        :return:
+        """
+        return get_object_or_404(Tag, pk=self.tag_id)
+
+# создание тега
+class TagCreateView(CreateView, NameContextMixin):
+    # form_class =
+    fields = '__all__'
+    model = Tag
+    success_url = reverse_lazy('blog:tag_list')
+    template_name = 'blogapp/tag_create.html'
+
+    def post(self, request, *args, **kwargs):
+        """
+        Пришел пост запрос
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        """
+        Метод срабатывает после того как форма валидна
+        :param form:
+        :return:
+        """
+        return super().form_valid(form)
+
+
+# class TagUpdataView(UpdateView):
+#     fields = '__all__'
+#     model = Tag
+#     success_url = reverse_lazy('blog:tag_list')
+#     template_name = 'blogapp/tag_create.html'
+#
+#
+# class TagDeleteView(DeleteView):
+#     template_name = 'blogapp/tag_delete_confirm.html'
+#     model = Tag
+#     success_url = reverse_lazy('blog:tag_list')
